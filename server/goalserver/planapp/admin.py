d3e54645900758c3fcda46e5faf1771cec816885
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-11-14 11:23:06 -0700

    admin site can re-direct you to planapp

--- server/goalserver/planapp/admin.py
+++ server/goalserver/planapp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Goal, Plan, Task
 
+admin.site.site_url = "/planapp"
+
 admin.site.register(Goal)
 admin.site.register(Plan)
 admin.site.register(Task)
