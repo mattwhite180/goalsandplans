9f529b9d697a2747e68ae03e7b6f10ef2f963d76
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-11-25 16:16:41 -0700

    no more /planapp/ in url

--- server/goalserver/goalserver/urls.py
+++ server/goalserver/goalserver/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('planapp/', include('planapp.urls')),
+    path('', include('planapp.urls')),
     path('admin/', admin.site.urls),
 ]
 handler404 = 'planapp.views.handler404'
