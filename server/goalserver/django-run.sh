025b72a8fd2f2ceeeeaf79deaa7b607bdce89df9
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-11-28 11:36:18 -0700

    dockerfile can be independent of docker-compose

--- server/goalserver/django-run.sh
+++ server/goalserver/django-run.sh
@@ -17,8 +17,12 @@ hostname -I
 hostname -I
 hostname -I
 hostname -I
+echo 'sleeping for 5'
+sleep 5
+echo 'done sleeping. running migrations'
 python3 manage.py makemigrations planapp && \
 python3 manage.py makemigrations
 python3 manage.py makemigrations && \
 python3 manage.py migrate && \
+echo 'done with migrations. running server'
 python3 manage.py runserver 0:8000
