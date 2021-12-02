202bc7ef8271279f6d1b873a51a21a3813e3ba10
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-12-01 21:08:50 -0700

    admin recognizes minitodo

--- server/goalserver/planapp/admin.py
+++ server/goalserver/planapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Goal, Plan, Task
+from .models import Goal, Plan, Task, MiniTodo
 
 admin.site.site_url = "/planapp"
 
@@ -11,9 +11,13 @@ class PlanAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'description', 'default_priority', 'goal')
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'id', 'description', 'priority', 'plan')
+    list_display = ('title', 'id', 'description', 'priority', 'minitodo', 'plan')
+
+class MiniTodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'user', 'description', 'priority')
 
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(Plan, PlanAdmin)
 admin.site.register(Task, TaskAdmin)
+admin.site.register(MiniTodo, MiniTodoAdmin)
 
