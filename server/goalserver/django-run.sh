5ac18859123b61b245af6d1189f688fb3a059b94
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-12-12 13:21:00 -0700

    refactored django secret handling

--- server/goalserver/django-run.sh
+++ server/goalserver/django-run.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
-
-FILE=djangosecret.txt
-
-if [ -f "$FILE" ]; then
-    echo $FILE "exists."
-else 
-    echo "$FILE does not exist."
-    python3 create-secret.py > $FILE
-    echo "created $FILE"
-fi
-
-hostname -I
-hostname -I
-hostname -I
-hostname -I
-hostname -I
-hostname -I
-hostname -I
-echo 'sleeping for 5'
-sleep 5
-echo 'done sleeping. running migrations'
-python3 manage.py makemigrations planapp && \
-python3 manage.py makemigrations
-python3 manage.py makemigrations && \
-python3 manage.py migrate && \
+# hostname -I
+# hostname -I
+# hostname -I
+# hostname -I
+# hostname -I
+# hostname -I
+# hostname -I
+# echo 'sleeping for 5'
+# sleep 5
+# echo 'done sleeping. running migrations'
+# python3 manage.py makemigrations planapp && \
+# python3 manage.py makemigrations
+# python3 manage.py makemigrations && \
+# python3 manage.py migrate && \
+# echo 'done with migrations. running server'
 python3 manage.py collectstatic
-echo 'done with migrations. running server'
 python3 manage.py runserver 0:8000
--- server/goalserver/goalserver/settings.py
+++ server/goalserver/goalserver/settings.py
@@ -16,6 +16,7 @@ import boto3
 import base64
 import json
 from botocore.exceptions import ClientError
+from django.core.management.utils import get_random_secret_key
 
 
 # get the AWS RDS login info from the AWS secrets manager
@@ -68,9 +69,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secretkeyfile = open("djangosecret.txt", "r")
-SECRET_KEY = secretkeyfile.read()
-secretkeyfile.close()
+if "DJANGOSECRET" in os.environ:
+    SECRET_KEY = os.environ["DJANGOSECRET"]
+else:
+    SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
