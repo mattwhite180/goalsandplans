f314219cd7c5d5df5f2efd3e58486a90bcb5933d
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-12-14 10:16:36 -0700

    crontask unit tests work

--- server/goalserver/planapp/tests.py
+++ server/goalserver/planapp/tests.py
@@ -4,6 +4,8 @@ from .models import Goal, Plan, Task
 from django.contrib.auth.models import AnonymousUser, User
 from .views import run_jobs
 from django.test import Client
+import datetime
+
 
 
 class CronTestCase(TestCase):
@@ -15,62 +17,51 @@ class CronTestCase(TestCase):
         )
         Plan.objects.create(
             title="test plan (continuous)",
-            description="this should increase the task count by 5",
+            description="This test should create 1 task",
             goal=g,
             continuous=True,
-            limit=5,
+            limit=1,
+            add_count=1,
+            add_period=1,
+        )
+        Plan.objects.create(
+            title="test plan (continuous)",
+            description="This test should not create any tasks",
+            goal=g,
+            continuous=False,
+            limit=10,
+            add_count=1,
+            add_period=1,
+        )
+        Plan.objects.create(
+            title="test plan (continuous - limit)",
+            description="This test should create 3 tasks",
+            goal=g,
+            continuous=True,
+            limit=3,
             add_count=5,
             add_period=1,
         )
         Plan.objects.create(
-            title="test plan (not continuous)",
-            description="this should not increase the task count",
-            goal=g,
-            continuous=False,
-            limit=700,
-            add_count=700,
-            add_period=1,
-        )
-        Plan.objects.create(
-            title="test plan (continuous outside of time)",
-            description="this should not increase the task count",
+            title="test plan (continuous - not yet time)",
+            description="This test should not create any tasks",
             goal=g,
             continuous=True,
-            limit=20000,
-            add_count=20000,
+            limit=1,
+            add_count=1,
             add_period=2,
-        )
-        p = Plan.objects.create(
-            title="test plan (continuous near limit)",
-            description="this should only increase the task count by 18",
-            goal=g,
-            continuous=True,
-            limit=20,
-            add_count=20,
-            add_period=1,
-        )
-        t = Task.objects.create(
-            title="task test1",
-            description="see if last plan will go over the limit",
-            plan=p,
-        )
-        t = Task.objects.create(
-            title="task test2",
-            description="see if last plan will go over the limit",
-            plan=p,
+            last_updated = datetime.date.today() - datetime.timedelta(days=1)
         )
 
     def test_sanity_check(self):
         errmsg = "if this fails, there is a syntax error in crontask, or a fatal runtime error"
         call_command("crontask")
-        val = 0
-        expected = 0
-        self.assertEqual(val, expected, errmsg)
+        self.assertEqual(0, 0, errmsg)
 
     def test_task_limit(self):
         call_command("crontask")
         val = Task.objects.count()
-        expected = 5 + 0 + 0 + 20
+        expected = 4
         errmsg = (
             "expected "
             + str(expected)
@@ -98,7 +89,7 @@ class CronTestCase(TestCase):
         )
         self.assertEqual(val, expected, errmsg)
         val = Task.objects.count()
-        expected = 5 + 0 + 0 + 20
+        expected = 4
         errmsg = (
             "expected "
             + str(expected)
