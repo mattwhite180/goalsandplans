f2e700559c0fda971b75101ec4efdae776807c61
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2022-01-01 17:58:25 -0700

    refactored cron task

--- server/goalserver/planapp/management/commands/crontask.py
+++ server/goalserver/planapp/management/commands/crontask.py
@@ -12,26 +12,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         created = 0
-        for plan in Plan.objects.all():
-            if plan.continuous:
-                deltaDate = datetime.date.today() - plan.last_updated
-                if deltaDate.days >= plan.add_period:
-                    plan.last_updated = datetime.date.today()
-                    plan.save()
-                    try:
-                        for i in range(plan.add_count):
-                            currentCount = Task.objects.filter(plan=plan).count()
-                            if currentCount < plan.limit:
-                                newT = Task.objects.create(
-                                    title=plan.recurring_task_title,
-                                    description=plan.recurring_task_description,
-                                    priority=plan.default_priority,
-                                    plan=plan,
-                                )
-                                newT.save()
-                                created += 1
-                    except:
-                        raise CommandError("error running crontask.py")
+        for plan in Plan.objects.filter(continuous=True):
+            deltaDate = datetime.date.today() - plan.last_updated
+            if deltaDate.days >= plan.add_period:
+                plan.last_updated = datetime.date.today()
+                plan.save()
+                try:
+                    for i in range(plan.add_count):
+                        currentCount = Task.objects.filter(plan=plan).count()
+                        if currentCount < plan.limit:
+                            newT = Task.objects.create(
+                                title=plan.recurring_task_title,
+                                description=plan.recurring_task_description,
+                                priority=plan.default_priority,
+                                plan=plan,
+                            )
+                            newT.save()
+                            created += 1
+                except:
+                    raise CommandError("error running crontask.py")
 
         self.stdout.write(self.style.SUCCESS(str(datetime.datetime.now())))
         self.stdout.write(
