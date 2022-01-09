46f16aa4766e22ce2b2db3771afca9edc7d77083
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2022-01-08 21:36:59 -0700

    cleaned up migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,3 @@
-docker exec -it goalsandplans_server_1 python3 manage.py clear_cache
-docker exec -it goalsandplans_server_1 python3 manage.py clean_pyc
-docker exec -it goalsandplans_server_1 python3 manage.py reset_schema
-docker exec -it goalsandplans_server_1 python3 manage.py reset_db
 docker exec -it goalsandplans_server_1 python3 manage.py migrate auth
 docker exec -it goalsandplans_server_1 python3 manage.py makemigrations
 docker exec -it goalsandplans_server_1 python3 manage.py migrate
