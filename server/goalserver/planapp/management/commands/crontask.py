a57d18899526dc2d325a8f48be24b0bab10c36cc
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2022-02-28 12:35:30 -0800

    added exception handling in crontask

--- server/goalserver/planapp/management/commands/crontask.py
+++ server/goalserver/planapp/management/commands/crontask.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AnonymousUser, User
 from django.core.management.base import BaseCommand, CommandError
 from planapp.models import Goal, Plan, Task, Issue
 from django.core.management import call_command
+from planapp.models import Issue
+
 
 import time
 
@@ -14,7 +16,15 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        WAIT_TIME = 1 * 60 * 5
-        while True:
-            call_command("taskify")
-            time.sleep(WAIT_TIME)
+        try:
+            WAIT_TIME = 1 * 60 * 5
+            while True:
+                call_command("taskify")
+                time.sleep(WAIT_TIME)
+        except Exception as e:
+            i = Issue.objects.create(
+                obj_info = "crontask: outside of taskify"
+                where = "crontask",
+                exception_string = str(e)
+            )
+            i.save()
