93c165c3ca83b4bde3435ac242c34dc0f60f7d06
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2022-02-27 19:04:34 -0800

    fixing taskify

--- server/goalserver/planapp/management/commands/taskify.py
+++ server/goalserver/planapp/management/commands/taskify.py
@@ -12,33 +12,30 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        WAIT_TIME = 1 * 60 * 60
-        while True:
-            for plan in Plan.objects.filter(continuous=True):
-                deltaDate = datetime.date.today() - plan.last_updated
-                if (deltaDate.days >= 1 and plan.today()) or plan.keep_at_limit:
-                    plan.last_updated = datetime.date.today()
-                    plan.save()
-                    try:
-                        for i in range(plan.add_count):
-                            currentCount = Task.objects.filter(plan=plan).count()
-                            if currentCount < plan.limit:
-                                newT = Task.objects.create(
-                                    title=plan.recurring_task_title,
-                                    description=plan.recurring_task_description,
-                                    priority=plan.default_priority,
-                                    plan=plan,
-                                    points=plan.default_points,
-                                )
-                                if plan.default_todolist:
-                                    newT.todolist = plan.default_todolist
-                                newT.save()
-                                created += 1
-                    except Exception as e:
-                        i = Issue.objects.create(
-                            obj_info = "Plan: " + str(plan.id),
-                            where = "taskify",
-                            exception_string = str(e)
-                        )
-                        i.save()
-            time.sleep(WAIT_TIME)
+        for plan in Plan.objects.filter(continuous=True):
+            deltaDate = datetime.date.today() - plan.last_updated
+            if (deltaDate.days >= 1 and plan.today()) or plan.keep_at_limit:
+                plan.last_updated = datetime.date.today()
+                plan.save()
+                try:
+                    for i in range(plan.add_count):
+                        currentCount = Task.objects.filter(plan=plan).count()
+                        if currentCount < plan.limit:
+                            newT = Task.objects.create(
+                                title=plan.recurring_task_title,
+                                description=plan.recurring_task_description,
+                                priority=plan.default_priority,
+                                plan=plan,
+                                points=plan.default_points,
+                            )
+                            if plan.default_todolist:
+                                newT.todolist = plan.default_todolist
+                            newT.save()
+                            created += 1
+                except Exception as e:
+                    i = Issue.objects.create(
+                        obj_info = "Plan: " + str(plan.id),
+                        where = "taskify",
+                        exception_string = str(e)
+                    )
+                    i.save()
